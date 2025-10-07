36337efb8235a654bcc903c45b424f37439a2ee6
jayant200803 <jayantunited20@gmail.com>
2025-10-07 16:33:10 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ app.use("/api", chatRoutes);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://vaibhavratnesh24:Turnuptheradio@cluster0.zglwqvx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        await mongoose.connect(process.env.MONGO_DB);
         console.log("Connected with Database!");
 
         app.listen(PORT, () => {
